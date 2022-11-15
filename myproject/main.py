2dd14b6b2c9a003b46d932bbd18010e2394e9290
wimadriaensen <r0445498@student.thomasmore.be>
2022-11-15 12:36:49 +0100

    correcte .py file toegevoegd + aanpassing workflow

--- myproject/main.py
+++ myproject/main.py
@@ -1,57 +1,74 @@
-#main.py
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Union  # Dit moet ik gebruiken omdat ik anders de "None | Nonen" niet kan gebruiken
+# Deze wordt vervangen door "Union[str, None] = None"
+import requests
+from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-
-#alle andere .py bestanden inladen (importeren)
-import crud
-import schemas
-import models
-from database import SessionLocal, engine
-
-import os    #Nodig om de map aan te maken --> SQLALCHEMY_DATABASE_URL = "sqlite:/// -->  ./sqlitedb/ <--   sqlitedata.db"
-
-if not os.path.exists('.\sqlitedb'):
-    os.makedirs('.\sqlitedb')
-
-
-models.Base.metadata.create_all(bind=engine)   # start de database op en pakt de data van models en create daar onze entiteiten uit
+class Course(BaseModel):
+    name_course: str
+    lecturer: str
 
 app = FastAPI()
 
-def get_db():
-    db = SessionLocal()    #Hier maak je de db aan die je overal in crud gebruikt
-    try:
-        yield db
-    finally:
-        db.close()
+origins = [
+    "http://localhost/",
+    "http://localhost:8080/",
+    "https://localhost.tiangolo.com/",
+    "http://127.0.0.1:5500/",
+    "https://wimadriaensen.github.io/"  # NIET VERGETEN OM HIER OOK DE LINK VAN UW hosted front-end ni bij te zetten!
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:         #is db_user null of is dit iets (opgevuld) zoja: ...
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int=0, limit: int=100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip, limit)
-    return users
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
 
 
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
-    return crud.create_item(db=db, item=item, user_id=user_id)
+course_api = {
+    "name_course": "API Development",
+    "lecturer": "Michiel Verboven"
+}
 
-@app.get("/items/", response_model=list[schemas.Item])
-def read_item(skip: int=0, limit: int=100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip, limit)
-    return items
+course_iot = {
+    "name_course": "IoT Advanced",
+    "lecturer": "Stef Van Wolputte"
+}
+
+courses = {}
+courses[0] = course_api
+courses[1] = course_iot
+
+
+@app.get("/courses")
+async def show_courses():
+    return courses
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+# Optie voor een extra GET? --> https://api.github.com/repos/wimadriaensen/wimadriaensen.github.io
+# bv. url van mijn github ophalen
+@app.get("/maker")
+async def show_maker():
+    response = requests.get("https://api.github.com/repos/wimadriaensen/wimadriaensen.github.io")
+    response_dict = {}
+    response_dict["owner"] = response.json()["owner"]["login"]
+    response_dict["github"] = response.json()["owner"]["html_url"]
+    response_dict["repository"] = response.json()["html_url"]
+    return response_dict
+
+@app.post("/courses")
+async def create_course(course: Course):
+    new_key = len(courses)
+    courses[new_key]= course
+    return courses[new_key]
+
